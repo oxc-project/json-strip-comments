e1bd92a442cb4772b73b0844059fd85ce0ea6002
Boshen <boshenc@gmail.com>
2025-02-21 09:42:21 +0800

    Rust 2024

--- benches/strip.rs
+++ benches/strip.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 pub fn bench(c: &mut Criterion) {
     c.bench_function("tsconfig", |b| {
